9cb0f80fce2bfd940d00369d8905cea78549e05e
kuiguo <kuiguo1991@gmail.com>
2017-08-28 11:08:00 +0800

    提交06

--- demo6/index.js
+++ demo6/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css'
 ReactDOM.render(
     <div>
-        <h3 className="h3">hello world!!</h3>
-        <h4 className="h4">hello world!!</h4>
+        <h1 className="h1">hello world!!!</h1>
+        <h2 className="h2">hello world!!!</h2>
+        <h3 className="h3">hello world!!!</h3>
+        <h4 className="h4">hello world!!!</h4>
     </div>,
     document.getElementById("root")
 );
