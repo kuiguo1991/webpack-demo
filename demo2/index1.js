2ec1ff367a59a0cc692864c79e1b5edf59c36fbc
kuiguo <kuiguo1991@gmail.com>
2017-09-07 13:59:21 +0800

    提交16

--- demo2/index1.js
+++ demo2/index1.js
@@ -1,3 +1,3 @@
-document.write("<h1>hello world!!!A</h1>");
-document.write("<h2>hello world!!!B</h2>");
-document.write("<h3>hello world!!!C</h3>");
+document.write("<h1>hello world!!!</h1>");
+document.write("<h2>hello world!!!</h2>");
+document.write("<h3>hello world!!!</h3>");
